f7e0fa163ca16b8ad1a3f6b1d540c978e45dc442
joan-gomez-r <joan.gomez-r@uniminuto.edu.co>
2024-04-27 08:25:22 -0500

    login config

--- config/params.php
+++ config/params.php
@@ -5,4 +5,7 @@ return [
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
     'bsVersion' => '5.x',
+    'mdm.admin.configs' => [
+        'defaultUserStatus' => 10, // 0 = inactive, 10 = active
+    ]
 ];
